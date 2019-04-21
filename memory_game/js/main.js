cae27bb62fa882b36a10b8c6ad21546553e89a30
Andrew <acwilliamson419@gmail.com>
2019-04-20 19:18:36 -0700

    completed game!

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,43 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  {
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'image/queen-of-hearts.png'
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'image/queen-of-diamonds.png'
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'image/king-of-hearts.png'
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: 'image/king-of-diamonds.png'
+}
+];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[3];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log(cardsInPlay);
-if (cardsInPlay.length === 2){
-  if(cardsInPlay[0] === cardsInPlay[1]){
-    alert("You Found a match!");
-  }else{
-    alert('Try again');
+function flipCard(cardId){
+  cardsInPlay.push(cards[cardID]);
+  console.log(cards[cardId].rank)
+  if (cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+      alert("You Found a match!");
+    }else{
+      alert('Try again');
+    }
   }
 }
+function createBoard() {
+  for (var i =0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+  }
+}
+console.log(createBoard);
